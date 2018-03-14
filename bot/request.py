4388cc7fafa00434837f6d422c33dc6d34814aa1
Niklas Korz <korz.niklask@gmail.com>
2018-03-14 17:04:12 +0100

    Use NamedTuple for Context and Request class

--- bot/request.py
+++ bot/request.py
@@ -1,19 +1,18 @@
+from typing import NamedTuple
 import json
 
 
-class Context(object):
-    def __init__(self, mood: float, affection: float):
-        self.mood = mood
-        self.affection = affection
+class Context(NamedTuple):
+    mood: float
+    affection: float
 
 
-class Request(object):
-    def __init__(self, content: str, timestamp: int, context: Context, bot_id: str, user_id: str):
-        self.content = content
-        self.timestamp = timestamp
-        self.context = context
-        self.bot_id = bot_id
-        self.user_id = user_id
+class Request(NamedTuple):
+    content: str
+    timestamp: int
+    context: Context
+    bot_id: str
+    user_id: str
 
 
 def parse_request(json_data: str):
