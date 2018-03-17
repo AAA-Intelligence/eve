23623563d33f951dbe35e154fcaf7cd361ea2c55
Niklas Korz <korz.niklask@gmail.com>
2018-03-17 11:24:09 +0100

    Simpler code for finding and loading pattern definition files (Python 3 ftw)

--- bot/patterns/__init__.py
+++ bot/patterns/__init__.py
@@ -1,21 +1,25 @@
 from typing import Dict, List
-from os import path, listdir
+from os import path
+from pathlib import Path
 from ..predefined_answers import Category
+
+dir = path.dirname(__file__)
+
+
+def pattern_file_for_category(category: Category):
+    p = Path(dir, category.name + '.txt')
+    if not p.is_file():
+        raise Exception(
+            'No pattern definition file found for category {}'.format(category))
+    return p.open()
 
 
 def get_patterns() -> Dict[Category, List[str]]:
     patterns = {}
-
-    dir = path.dirname(__file__)
-    for file_name in listdir(dir):
-        if file_name.endswith('.txt'):
-            with open(file_name) as f:
-                # Retrieve category name without file extension
-                name = file_name.replace('.txt', '')
-                if name not in Category:
-                    raise Exception(
-                        'Found pattern definition for invalid category {}'.format(name))
-                category = Category[name]
-                # Store all lines of the pattern file in the dictionary, associated to
-                # the specific category by treating f as an iterator over the file's lines
-                patterns[category] = list(f)
+    # Iterate over all defines categories
+    for category in Category:
+        # Try to the open pattern definition file for the category
+        with pattern_file_for_category(category) as f:
+            # Store all lines of the pattern file in the dictionary, associated to
+            # the specific category by treating f as an iterator over the file's lines
+            patterns[category] = list(f)
