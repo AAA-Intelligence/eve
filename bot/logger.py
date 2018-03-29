ab040756d4b93fec46a484cf4d682211af5c2818
Niklas Korz <korz.niklask@gmail.com>
2018-03-29 11:06:24 +0200

    Use spaces in Python for indentation

--- bot/logger.py
+++ bot/logger.py
@@ -5,7 +5,7 @@ from sys import stderr
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 file_handler = logging.FileHandler(
-	path.join(path.dirname(__file__), 'bot.log'))
+    path.join(path.dirname(__file__), 'bot.log'))
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
--- bot/training_data.py
+++ bot/training_data.py
@@ -2,10 +2,10 @@ from typing import NamedTuple, List
 
 
 class TrainingData(NamedTuple):
-	"""
-	Data type for training data that will be saved after training,
-	used by the pattern recognizer to access the list of all stems
-	"""
-	total_stems: List[str]
-	train_x: List[int]
-	train_y: List[int]
+    """
+    Data type for training data that will be saved after training,
+    used by the pattern recognizer to access the list of all stems
+    """
+    total_stems: List[str]
+    train_x: List[int]
+    train_y: List[int]
