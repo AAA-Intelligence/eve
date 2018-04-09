e51a332ee33128fa4f27a4126f0294703c5aa4ad
Niklas Korz <korz.niklask@gmail.com>
2018-04-09 21:09:44 +0200

    Set tensorflow logging to info for chat training

--- bot/text_processor/train.py
+++ bot/text_processor/train.py
@@ -1,10 +1,12 @@
 from os import path, makedirs
 
 from opennmt.runner import Runner
+import tensorflow as tf
 
 from bot.text_processor.setup import config, model
 
 
 def train_and_evaluate():
+    tf.logging.set_verbosity(tf.logging.INFO)
     runner = Runner(model, config)
     runner.train_and_evaluate()
