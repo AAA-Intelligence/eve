852502e4c4bbaeea46cc448fcd914ce2bb1ef5e9
Simon Niedermayr <simon.niedermayr@gmx.de>
2018-04-16 16:00:30 +0200

    Added bot sorting

--- db/bot.go
+++ db/bot.go
@@ -51,6 +51,7 @@ type Bot struct {
 	FatherAge     int
 	MotherName    int
 	MotherAge     int
+	CreationDate  time.Time
 }
 
 // Create creates a bot entry in the database
@@ -64,10 +65,11 @@ func (b *Bot) Create() error {
 	b.FatherAge = rand.Intn(20) + 40
 	b.MotherName = randomName(Female)
 	b.MotherAge = b.FatherAge + rand.Intn(10) - 5
+	b.CreationDate = time.Now()
 
 	v, err := dbConnection.db.Exec(`
-		INSERT INTO Bot(Name,Image,Gender,User,Affection,Mood,Pattern,Birthdate,FavoriteColor,FatherName,FatherAge,MotherName,MotherAge) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)`,
-		b.Name, b.Image, b.Gender, b.User, b.Affection, b.Mood, b.Pattern, b.Birthdate, b.FavoriteColor, b.FatherName, b.FatherAge, b.MotherName, b.MotherAge)
+		INSERT INTO Bot(Name,Image,Gender,User,Affection,Mood,Pattern,Birthdate,FavoriteColor,FatherName,FatherAge,MotherName,MotherAge,CreationDate) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)`,
+		b.Name, b.Image, b.Gender, b.User, b.Affection, b.Mood, b.Pattern, b.Birthdate, b.FavoriteColor, b.FatherName, b.FatherAge, b.MotherName, b.MotherAge, b.CreationDate)
 	if err != nil {
 		log.Println("error inserting new bot:", err)
 		return ErrInternalServerError
